f710ea230365fa88d3f50b0fe0a1e2685250b73b
maithe heloise <maithe.faria@escola.pr.gov.br>
2025-08-13 08:17:09 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,23 @@
+criaCartao(
+  'Gilmore Girls',
+  'Qual o nome do café em Stars Hollow?',
+  'O nome do café é Luke\'s Diner'
+)
 
+criaCartao(
+  'Gilmore Girls',
+  'Quem é a melhor amiga da Rory na escola Chilton?',
+  'A melhor amiga da Rory na Chilton é a Paris Geller'
+)
+
+criaCartao(
+  'Gilmore Girls',
+  'Qual o nome do primeiro namorado da Rory?',
+  'O nome do primeiro namorado da Rory é Dean Forester'
+)
+
+criaCartao(
+  'Gilmore Girls',
+  'Qual o nome da pousada que Lorelai gerencia e, mais tarde, se torna sua?',
+  'A pousada se chama Dragonfly Inn'
+)
