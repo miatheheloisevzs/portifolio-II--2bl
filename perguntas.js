714f791e08591a200cad3db57438f8ad88284c19
maithe heloise <maithe.faria@escola.pr.gov.br>
2025-08-20 08:07:18 -0300

    salvar

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,20 @@ criaCartao(
   'Qual o nome da pousada que Lorelai gerencia e, mais tarde, se torna sua?',
   'A pousada se chama Dragonfly Inn'
 )
+
+criaCartao(
+'Gilmore Girls',
+'Quem é o pai da filha da Sookie, a chef de cozinha do Dragonfly Inn?',
+'O pai é o Jackson Belleville'
+)
+
+criaCartao(
+'Gilmore Girls',
+'Qual o nome da cidade fictícia em que se passa a série?',
+'A cidade se chama Stars Hollow'
+
+criaCartao(
+'Gilmore Girls',
+'Em qual ano foi ao ar o primeiro episódio da série?',
+'O primeiro episódio da série foi ao ar em 2000'
+)
